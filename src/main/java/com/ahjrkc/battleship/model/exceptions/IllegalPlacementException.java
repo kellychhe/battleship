614fac2b82a142cbb3e170cf2ee220032818d0cb
JulianRDev <rosajulian1930@gmail.com>
2022-08-08 18:07:47 -0400

    Added exception package and class files.

---
+++ src/main/java/com/ahjrkc/battleship/model/exceptions/IllegalPlacementException.java
@@ -0,0 +1,8 @@
+package com.ahjrkc.battleship.model.exceptions;
+
+public class IllegalPlacementException {
+
+// When someone tries to place a ship where another ships are already placed
+  // Also when trying to place out of bounds
+
+}
