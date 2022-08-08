f3f70cf8e2b454414b2e5eee125bc641f27c4a6a
JulianRDev <rosajulian1930@gmail.com>
2022-08-08 17:42:22 -0400

    added methods to State Enum.

--- src/main/java/com/ahjrkc/battleship/model/State.java
+++ src/main/java/com/ahjrkc/battleship/model/State.java
@@ -2,10 +2,32 @@ package com.ahjrkc.battleship.model;
 
 public enum State {
 
-  PLAYER_MOVE,
-  COMPUTER_MOVE,
+  PLAYER_MOVE{
+    @Override
+    public boolean isCurrentTurn(){return true;};
+
+    @Override
+    public State nextMove(){return COMPUTER_MOVE;}
+
+    @Override
+    public State winState(){return PLAYER_WIN;}
+  },
+  COMPUTER_MOVE{
+    @Override
+    public boolean isCurrentTurn(){return true;};
+
+    @Override
+    public State nextMove(){return PLAYER_MOVE;}
+
+    @Override
+    public State winState(){return COMPUTER_WIN;}
+  },
   PLAYER_WIN,
   COMPUTER_WIN;
 
+  public boolean isCurrentTurn(){return false;};
 
+  public State nextMove(){return null;}
+
+  public State winState(){return null;}
 }
