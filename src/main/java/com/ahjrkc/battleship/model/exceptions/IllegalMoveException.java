8e19bedd7e5cea45536f254f2569b9e916e2b4fd
JulianRDev <rosajulian1930@gmail.com>
2022-08-08 18:30:36 -0400

    added to IllegalMoveException and created constant for the message.

--- src/main/java/com/ahjrkc/battleship/model/exceptions/IllegalMoveException.java
+++ src/main/java/com/ahjrkc/battleship/model/exceptions/IllegalMoveException.java
@@ -3,7 +3,7 @@ package com.ahjrkc.battleship.model.exceptions;
 public class IllegalMoveException extends IllegalArgumentException {
 
   // when player tries hitting ship that has already been sunk
-  
+
   public IllegalMoveException(String message){
     super(message);
   }
