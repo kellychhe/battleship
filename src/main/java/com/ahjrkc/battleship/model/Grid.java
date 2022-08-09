c4ea7635bcccec4a1e1e8028e9dbf5c7da963e38
kellichh <kellykchhe@gmail.com>
2022-08-08 21:59:44 -0400

    modified all Grid classes

--- src/main/java/com/ahjrkc/battleship/model/Grid.java
+++ src/main/java/com/ahjrkc/battleship/model/Grid.java
@@ -2,10 +2,25 @@ package com.ahjrkc.battleship.model;
 
 import static com.ahjrkc.battleship.model.Game.*;
 
+import java.security.SecureRandom;
+import java.util.Random;
+
 public interface Grid {
 
-  MarkerType[][] grid = new MarkerType[NUM_OF_ROWS][NUM_OF_COLS];
+  Ships ships[] = Ships.values();
+  ShipDirection shipDirection[] = ShipDirection.values();
+  Random rng = new SecureRandom();
 
-  void setGrid();
+  static Ships[][] createShipsGrid() {
+    return new Ships[NUM_OF_ROWS][NUM_OF_COLS];
+  }
 
+  static MarkerType[][] createMarkerGrid() {
+    return new MarkerType[NUM_OF_ROWS][NUM_OF_COLS];
+  }
+
+
+  Ships[][] setShipsGrid();
+
+  int[] shipIndex(Ships ship);
 }
--- src/main/java/com/ahjrkc/battleship/model/PlayerGrid.java
+++ src/main/java/com/ahjrkc/battleship/model/PlayerGrid.java
@@ -1,10 +1,18 @@
 package com.ahjrkc.battleship.model;
 
+
+
 public class PlayerGrid implements Grid{
 
+  Ships[][] shipsGrid;
   @Override
-  public void setGrid() {
+  public Ships[][] setShipsGrid() {
+    return new Ships[0][];
+  }
 
+  @Override
+  public int[] shipIndex(Ships ship) {
+    return new int[0];
   }
 
 }
