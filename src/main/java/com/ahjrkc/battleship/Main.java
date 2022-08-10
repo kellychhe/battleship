3496c05c838edd5c7f99b0c38b20d1ba83ced58d
DevHarleyA <aharleydev@gmail.com>
2022-08-10 10:28:13 -0400

    worked on controller, created method to welcome user and get coordinates. Stubbed out method for ship placement. Mediator class will eventually be deleted.

--- src/main/java/com/ahjrkc/battleship/Main.java
+++ src/main/java/com/ahjrkc/battleship/Main.java
@@ -1,10 +1,16 @@
 package com.ahjrkc.battleship;
 
+import com.ahjrkc.battleship.controller.Game;
+import java.io.IOException;
+
 public class Main {
 
-  public static void main(String[] args) {
-
-    }
+  public static void main(String[] args) throws IOException {
+    // testing methods from Game Class
+    Game game = new Game();
+    game.greetPlayer();
+    game.grabUserCoordinates();
+  }
 
 
 }
--- src/main/java/com/ahjrkc/battleship/controller/Game.java
+++ src/main/java/com/ahjrkc/battleship/controller/Game.java
@@ -2,21 +2,65 @@ package com.ahjrkc.battleship.controller;
 
 import com.ahjrkc.battleship.model.Board;
 import com.ahjrkc.battleship.model.ShipType;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.util.ArrayList;
+import java.util.regex.Pattern;
 
+/**
+ *
+ */
 public class Game {
 
   public static final int NUM_OF_ROWS = 10;
   public static final int NUM_OF_COLS = 10;
+  // Do not delete below, helps with filtering user coordinates.
+  private static final Pattern INPUT_SPLITTER = Pattern.compile("\\D+");
 
   public int[] shot;
   public ShipType[] fleet;
   // loop through ShipType array to check that all ships are placed for both player and cpu
+  public String playerName;
 
   Board player = new Board();
   Board cpu = new Board();
 
+  public void greetPlayer() throws IOException {
+    BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
+    System.out.println("Ahoy there Matey! What is your name?");
+    playerName = buffer.readLine().trim();
+    System.out.printf("Welcome to Battleship, %s", playerName);
+  }
+
   // get the ship placement from reader :D
+  public void playerSetShips() {
+    throw new UnsupportedOperationException("Not yet implemented");
+  }
+
+  // grab coordinates from user and return shot to be used in other methods
+  public int[] grabUserCoordinates() throws IOException {
+    Reader reader = new InputStreamReader(System.in);
+    BufferedReader buffer = new BufferedReader(reader);
+    do {
+      System.out.println();
+      System.out.printf("Ay ay %s, Please enter your coordinates.", playerName);
+      System.out.println();
+      System.out.println("Coordinates must be two numbers between 0-9 separated by a comma.");
+      String coordinates = buffer.readLine().trim();
+      shot = INPUT_SPLITTER
+          .splitAsStream(coordinates)
+          .map(String::trim)
+          .filter((s) -> !s.isEmpty())
+          .mapToInt(Integer::parseInt)
+          .filter((value) -> value >= 0 && value < 10)
+          .limit(2)
+          .toArray();
+    } while (shot.length == 0);
+
+    return shot;
+  }
 
   // use do while loop to keep coordinates in bounds, repeat prompting if they dont get it
 
@@ -30,13 +74,11 @@ public class Game {
   // prompt reader for first shot (coordinates)
 
   // process each hit(Ships Class) on the cpu to see if cpu was hit
-    // if it returns true run areAllShipsSunk (Board Class), then REPEAT_TURN(State) for player
+  // if it returns true run areAllShipsSunk (Board Class), then REPEAT_TURN(State) for player
 
   // if false run addMiss(Board Class) for player
 
   // Then change state to CPU_TURN(State) :)
-
-
 
   // Have a throws clause for IllegalPlacementException (extends IllegalArgumentException (checked)
   // Have a throws clause for GameOverException (extends IllegalStateException) (checked)
