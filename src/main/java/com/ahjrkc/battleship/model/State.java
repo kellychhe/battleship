7e4d5f7c24548307fe48de9c85a141e75a279129
JulianRDev <rosajulian1930@gmail.com>
2022-08-11 00:12:28 -0400

    added javadoc to state.

--- src/main/java/com/ahjrkc/battleship/model/State.java
+++ src/main/java/com/ahjrkc/battleship/model/State.java
@@ -1,8 +1,17 @@
 package com.ahjrkc.battleship.model;
 
-public enum State {
+/**
+ *<p>This Enum is to determine the state in which the game is in at all times.</p>
+ * <ul>
+ *   <li>PLACEMENT: this state is for the beginning of the game when the player and cpu ships are being placed</li>
+ *   <li>PLAYER_MOVEMENT: this state is for when the player is taking shots at the cpu or essentially when it is the players turn</li>
+ *   <li>CPU_MOVEMENT: this state is for when the cpu is taking shots ate the player or essentially it is the cpu turn</li>
+ *   <li>PLAYER_WIN: this state is to determine the game has ended with the player being the winner</li>
+ *   <li>CPU_WIN: this state is to determine the game has ended with the cpu being the winner</li>
+ * </ul>
+ */
 
-  
+public enum State {
 
   // game begins as user placing ship (computer can place ships first or after)
 
