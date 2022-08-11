f18c85d0f3f24035b4c41dae299e3754e6a1be93
DevHarleyA <aharleydev@gmail.com>
2022-08-11 09:40:40 -0400

    Ship class javadoc completed

--- src/main/java/com/ahjrkc/battleship/model/Ship.java
+++ src/main/java/com/ahjrkc/battleship/model/Ship.java
@@ -5,14 +5,8 @@ import java.util.Arrays;
 import java.util.List;
 
 /**
- * <p></p>
- *<ul>
- *   <li>processEachHit: checks if the shot has hit a ship by iterating through each ships coordinates
- *   to find possible matches. It changes the boolean at the corresponding index of the
- *   hitTracker ArrayList if a match is found</li>
- *   <li>isSunk: Iterates through an individual ship placement to see if all sections in ship
- *   hitTracker field were hit(all true), returns boolean of whether or not that ship has been sunk.</li>
- *</ul>
+ * <p>The Ship class is used to instantiate a new ship of each type for the player and the computer. In the
+ * beginning of the game, the fleet of ships are created when the user and computer set their ships on the board.</p>
  */
 
 public class Ship {
@@ -23,6 +17,14 @@ public class Ship {
 
   public List<Boolean> hitTracker = new ArrayList<>();
 
+  /**
+   * Creates a new Ship of each type for the player and computer when a new placement is established
+   * the Game class.
+   *
+   * @param type      specifies the type of ship being placed.
+   * @param placement these are the coordinates specified either by the player or randomly by
+   *                  the computer to designate the ship's placement on the board.
+   */
   public Ship(ShipType type, ArrayList<int[]> placement) {
     this.type = type;
     this.placement = placement;
@@ -31,8 +33,14 @@ public class Ship {
       hitTracker.add(false);
     }
   }
-  // leave orientation to the controller
 
+  /**
+   * This method checks if a shot has hit a ship by iterating through each ship's coordinates
+   * to find possible matches. It changes the boolean at the corresponding index of the
+   * hitTracker ArrayList if a match is found.
+   * @param shot an array of integer coordinates in the order of row and column.
+   * @return a boolean
+   */
   public boolean processEachHit(int[] shot) {
     for (int i = 0; i < placement.size(); i++) {
       if (Arrays.equals(placement.get(i), shot)) {
@@ -41,13 +49,13 @@ public class Ship {
       }
     }
     return false;
-    // using the boolean, we can react to it / do something with it
-    // return true if hasnt
-    // loop through all placements and check to see if any placements match row and col of the hit
-    // if hit, update hitTracker, return true
-    // return false
   }
 
+  /**
+   * This method iterates through an individual ship placement to see if all sections in ship
+   * hitTracker field were hit(all true), returns boolean of whether that ship has been sunk.
+   * @return a boolean
+   */
   public boolean isSunk() {
     for (Boolean section : hitTracker) {
       if (section.equals(false)) {
@@ -55,8 +63,6 @@ public class Ship {
       }
     }
     return true;
-    // loop through hit tracker
-    // if all true, return true, else return false
   }
 
 
@@ -84,5 +90,4 @@ public class Ship {
     this.hitTracker = hitTracker;
   }
 
-  // add toString
 }
